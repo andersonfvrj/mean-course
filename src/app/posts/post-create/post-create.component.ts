990b286408bd4c3d1c4dcd1dbe27bde625cd2039
Anderson Vasconcelos <anderson.vasconcelos@capgemini.com>
2024-10-23 19:17:24 -0300

    Dia 02 - Até aula 26

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Post } from '../post.model';
 
 @Component({
@@ -12,11 +13,13 @@ export class PostCreateComponent {
   enteredContent = '';
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPosts() {
-    const post: Post = {
-      title: this.entederTitle,
-      content: this.enteredContent
+  onAddPosts(form: NgForm) {
+    if (form.valid) {
+      const post: Post = {
+        title: form.value.title,
+        content: form.value.content
+      }
+      this.postCreated.emit(post);
     }
-    this.postCreated.emit(post);
   }
 }
